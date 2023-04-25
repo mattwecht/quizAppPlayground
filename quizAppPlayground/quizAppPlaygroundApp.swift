959c69df29e3e9d45a9cd355717820b79c60306b
Matt Wecht <mattwecht@gmail.com>
2023-04-25 14:33:28 -0400

    Initial Commit

---
+++ quizAppPlayground/quizAppPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizAppPlaygroundApp.swift
+//  quizAppPlayground
+//
+//  Created by scholar on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct quizAppPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
